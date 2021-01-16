8c0fa9d4f3db44f58b6bd01caf481106dbd8ba34
asaqui94 <arpitasaqui@gmail.com>
2021-01-16 13:46:15 -0500

    final product

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,56 +1,61 @@
 class parkingGarage():
 
-    def __init__(self,payment,parkingSpaces,tickets):
-        self.payment = payment
-        self.parkingSpaces = parkingSpaces
+    def __init__(self,tickets,parkingSpaces,currentTicket):
         self.tickets = tickets
-   
-     # Get Ticket
-    def takeTicket(self):
-        if self.parkingSpaces == []:
-            print('Please wait, the garage is full.') 
-        else:
-            print(f'This is your parking spot: {tickets[-1]}')
-            user_ticket = self.tickets.pop()
-            return user_ticket
-            user_space = self.parkingSpaces.pop()
-            return 
-   
-    # Leave
-    def leaveGarage(self):
-        if self.payment == 'paid':
-            print('Thank you, have a nice day!')
-            self.tickets.append()
-            self.parkingSpaces.append()
-        else:
-            pay_now = input('Please enter your payment to leave.')
-            return pay_now
-            if self.payment == 'paid':
-                print('Thank you, have a nice day!')
+        self.parkingSpaces = parkingSpaces
+        self.currentTicket=currentTicket
+        
+    
 
+      # Get Ticket
+    def takeTicket(self):
+        if self.tickets == []:
+            return 'Sorry, the garage is full'
+        else:
+            print('Your ticket is number: ',self.tickets[-1])
+            self.tickets.pop(-1)
+            print('Your allocated parking space is : ',self.parkingSpaces[-1])
+            self.parkingSpaces.pop(-1)
+
+   
+   
     # Pay for Parking
     def payForParking(self):
-        if paid = True:
-            print('You have 15 mins to leave')
-        else:
-            self.payment = input('Please enter your payment: ')
-            currentTicket[self.tickets] = 'paid'
+        payment=''
+        while payment == '':
+            payment = input('Please enter any key to process your payment: ' )
+            if payment != '':
+                print('Your ticket has been paid. You have 15 mins to leave.')
+                self.currentTicket['paid']=True
+                return self.leaveGarage()
+            else:
+                continue
     
+    # Leave
+    def leaveGarage(self):
+        if self.currentTicket['paid']==True:
+            self.parkingSpaces.append(len(self.parkingSpaces)+1)
+            self.tickets.append(len(self.tickets)+1)
+            print('Thank You, have a nice day')
+        else:
+            self.payForParking()
 
 tickets = [1,2,3,4,5,6,7,8,9,10]
 parkingSpaces = [1,2,3,4,5,6,7,8,9,10]
-currentTicket = parkingGarage({})
+currentTicket={'paid':False}
+
+car=parkingGarage(tickets,parkingSpaces,currentTicket)
 
 def start_Garage():
     while True:
         print('What do you want to do?')
         response = input('Choose one of the following: Ticket/Pay/Leave ')
-        if response == 'Ticket' or 'ticket':
-            currentTicket.takeTicket()
-        elif response == 'Pay' or 'pay':
-            currentTicket.payForParking()
-        elif response == 'Leave' or 'leave':
-            currentTicket.leaveGarage()
+        if response.lower() == 'ticket':
+            car.takeTicket()
+        elif response.lower() == 'pay':
+            car.payForParking()
+        elif response.lower() == 'leave':
+            car.leaveGarage()
             break
         else:
             print('Please enter a valid input.')
